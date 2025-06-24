84d1c703f3405c34a913b41ccee2f625e990944a
winshine0326 <shane10340@gmail.com>
2025-06-24 10:29:02 +0900

    feat :: 3rd_section

--- lib/pages/3rd_section.dart
+++ lib/pages/3rd_section.dart
@@ -6,9 +6,8 @@ import 'package:subpersona/components/button.dart';
 import 'package:subpersona/components/top_layout.dart';
 import 'package:subpersona/pages/home.dart';
 import 'package:subpersona/components/task_btn.dart';
-import 'package:subpersona/pages/3rd_section.dart';
 
-class SecondSection extends StatelessWidget {
+class ThirdSection extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
@@ -24,21 +23,21 @@ class SecondSection extends StatelessWidget {
                     crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
                       TopLayout(
-                        title: "‘안녕하세요' 를 읽어보세요",
-                        subtitle: "2단계",
-                        imgPath: "assets/Rock2.png",
+                        title: "얼굴 사진을 찍어보세요",
+                        subtitle: "3단계",
+                        imgPath: "assets/Box.png",
                         padding: 52,
                         titleFont: 35,
                       ),
                       SizedBox(height: 100),
 
                       TaskBtn(
-                          ImgPath:"assets/mic.png"
+                          ImgPath:"assets/photo.png"
                       ),
                       SizedBox(height: 88),
                       Center(
                         child: AnalyzeButton(
-                          nextPage: ThirdSection(),
+                          nextPage: HomePage(),
                           label: '다음으로',
                         ),
                       )
